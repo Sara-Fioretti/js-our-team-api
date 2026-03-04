38755e2b104d20a5ebb361cf7711053e58d2ebe6
Sara-Fioretti <fiorettisara22@gmail.com>
2026-03-04 12:06:36 +0100

    Creazione card

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*PROCEDIMENTO
 1)Collego la libreria axios
-2)Prendo gli oggetti tramite api
-3)Aggancio il teamContainer mediante una query selector
+2)Aggancio il teamContainer mediante una query selector 
+3)Prendo gli oggetti tramite api
 4) Creo una funzione createCard
   4.1) Creo un div cardContainer con il comando createElement e con il comando appendChild lo appendo al teamContainer
   4.2) Creo un div imageContainer con il comando createElement e con il comando appendChild lo appendo al teamContainer
@@ -15,3 +15,65 @@
   3.2) Richiamo la funzione createCard
   */
 
+let api = "https://boolean-teachers.github.io/mock/api/members/"
+
+teamContainer = document.querySelector(".team-container")
+
+axios.get(api).then(response => {
+    let members = response.data;
+    members.forEach(teamMember => {
+        let { name, role, email, img } = teamMember
+        createCard(name, role, email, img)
+    })
+})
+
+function createCard(name, role, email, img) {
+    let cardContainer = document.createElement("div")
+    cardContainer.classList.add("team-card")
+    teamContainer.appendChild(cardContainer)
+
+    let imageContainer = document.createElement("div")
+    cardContainer.classList.add("card-image")
+    cardContainer.appendChild(imageContainer)
+
+    let memberImage = document.createElement("img")
+    memberImage.src = img
+    imageContainer.appendChild(memberImage)
+
+    let textContainer = document.createElement("div")
+    textContainer.classList.add("card-text")
+    cardContainer.appendChild(textContainer)
+
+    let memberName = document.createElement("h3")
+    memberName.innerText = name.toUpperCase()
+    textContainer.appendChild(memberName)
+
+    let memberRole = document.createElement("h4")
+    memberRole.innerText = role
+    textContainer.appendChild(memberRole)
+
+    let memberEmail = document.createElement("p")
+    memberEmail.innerText = email
+    textContainer.appendChild(memberEmail)
+}
+
+
+
+/*4) BONUS: Aggiungere un form di aggiunta membri che permetta di visualizzare il nuovo membro sulla pagina
+-) Aggancio il form e i vari elementi del form
+-) Aggiungo un addEventListner al form all'evento del submit 
+-) Agiungo al form un preventDefault
+-)Richiamo la funzione createCard
+*/
+let form = document.getElementById("new-member-form")
+let newMemberName = document.getElementById("newMemberName")
+let newMemberRole = document.getElementById("newMemberRole")
+let newMemberEmail = document.getElementById("newMemberEmail")
+let newMemberImage = document.getElementById("newMemberImage")
+
+form.addEventListener("submit",
+    function (e) {
+        e.preventDefault();
+        createCard(newMemberName.value, newMemberRole.value, newMemberEmail.value, newMemberImage.value)
+    }
+)
